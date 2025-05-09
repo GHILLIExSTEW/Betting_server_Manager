8853400b6a0080882f010d1768e88f1026aedc18
GHILLIExSTEW <93230904+GHILLIExSTEW@users.noreply.github.com>
2025-05-09 10:15:37 -0400

    Update main.py

--- betting-bot/main.py
+++ betting-bot/main.py
@@ -5,6 +5,7 @@ import sys
 import logging
 import discord
 from discord.ext import commands
+from discord import app_commands
 from dotenv import load_dotenv
 import asyncio
 from typing import Optional
